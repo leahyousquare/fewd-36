a0a070b95e826dcf890e8a04f442124e281090b6
Leah Filamor <leahfilamor@gmail.com>
2015-10-28 00:21:24 -0700

    updated the css

--- temp-calc/js/javascript.js
+++ temp-calc/js/javascript.js
@@ -1,13 +1,15 @@
 
-var tempF = document.querySelector('.temp-f');
-var tempC = document.querySelector('.temp-c');
+var tempF = document.querySelector(".temp-f");
+var tempC = document.querySelector(".temp-c");
+var startOver = document.querySelector(".start-again")
 
 tempF.addEventListener('keyup', calcFtoC);
 tempC.addEventListener('keyup', calcCtoF);
+startOver.addEventListener('click', redo);
 
 function calcFtoC(event) {
   if (tempF.value.length > 0) {
-    tempC.value = calcFahrenheitToCelcius(tempF.value)
+    tempC.value = fToC(tempF.value)
   } else {
     tempC.value = '';
   }
@@ -15,18 +17,24 @@ function calcFtoC(event) {
 
 function calcCtoF(event) {
   if (tempC.value.length > 0) {
-    tempF.value = calcCelciusToFahrenheit(tempC.value)
+    tempF.value = cToF(tempC.value)
   } else {
     tempF.value = '';
   }
 }
 
- function calcCelciusToFahrenheit(tempC) {
+ function cToF(tempC) {
         var tempF = tempC * (9/5) + 32;
         return tempF; 
       }
       // farenheit to celcius;
-        function calcFahrenheitToCelcius(tempF) {
+        function fToC(tempF) {
           var tempC = (tempF-32) * (5/9);
           return tempC;
       } 
+
+function redo(event) {
+  tempF.value = '';
+  tempC.value ='';
+
+  }
