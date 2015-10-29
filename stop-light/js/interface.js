0e90760ff2a5fe681a7c9fe38cf8bfbd3412ea00
Leah Filamor <leahfilamor@gmail.com>
2015-10-28 21:12:06 -0700

    adding stop light project

---
+++ stop-light/js/interface.js
@@ -0,0 +1,31 @@
+var color1 = document.querySelector(".red");
+var color2 = document.querySelector(".yellow");
+var color3 = document.querySelector(".green");
+var button1 = document.querySelector(".stp");
+var button2 = document.querySelector(".slw");
+var button3 = document.querySelector(".g");
+var body = document.querySelector("body");
+
+
+// when you click button 1, you want color1 
+// when you click button 2, you want color 2
+// when yu click button 3, you want color 3
+button1.addEventListener("click", changeRed);
+button2.addEventListener("click", changeYellow);
+button3.addEventListener("click", changeGreen);
+
+
+// when you want to change to red
+
+function changeRed(event) {
+	body.className = "stop";
+}
+
+function changeYellow(event) {
+	body.className = "slow";
+}
+
+function changeGreen(event) {
+	body.className = "go";
+
+}
