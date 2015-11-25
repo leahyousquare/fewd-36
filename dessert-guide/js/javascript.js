74d2eae94124b7aabae1959f440cadf10753dcdb
Leah Filamor <leahfilamor@gmail.com>
2015-11-25 15:48:56 -0800

    update lml

--- dessert-guide/js/javascript.js
+++ dessert-guide/js/javascript.js
@@ -1,13 +1,23 @@
-	var map;
 	function initMap() {
-	  map = new google.maps.Map(document.getElementById('map'), {
+	  var map = new google.maps.Map(document.getElementById('map'), {
 	    center: {lat: 37.7616486, lng: -122.4866023},
 	    zoom: 13
 	  });
-	}
+	
 
-	 var ctaLayer = new google.maps.KmlLayer({
-    url: 'kml/desserts.kml',
-    map: map
-  });
+var layer = new google.maps.KmlLayer ({
+	url: 'http://leahyousquare.github.io/fewd-36/dessert-guide/kml/desserts.kml',
+	map: map
+});
+
+
+
+// var myCenter = new google.maps.LatLng(37.7616486, -122.4866023);
+// var marker= new google.maps.Marker({
+//   position:myCenter,
+//   });
+
+// marker.setMap(map);
+
+// google.maps.event.addDomListener(window, 'load', initialize);
 }
