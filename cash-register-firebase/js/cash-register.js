544cbedfe32615a258119520c7178138fad18fd2
Leah Filamor <leahfilamor@gmail.com>
2015-12-01 01:00:49 -0800

    added markers

---
+++ cash-register-firebase/js/cash-register.js
@@ -0,0 +1,90 @@
+// Structure
+// ------------------------------------------------
+var form    = document.querySelector("form");
+var input   = document.querySelector("input");
+var entries = document.querySelector(".entries");
+var total   = document.querySelector(".total");
+
+
+// Setup
+// ------------------------------------------------
+var totalValue = 0;
+
+var receipt = {
+	"items": []
+}
+
+
+var firebase = new Firebase("https://fewd-cash-register.firebaseio.com/");
+
+// Events
+// ------------------------------------------------
+form.addEventListener("submit", enter);
+window.addEventListener("load", pageLoad);
+
+function dataChanged(snapshot) {
+	if (snapshot.val() === null) {
+		return;
+
+	totalValue = 0;
+	entries.innerHTML  = "";
+
+	receipt = snapshot.val();
+	receipt.items.forEach(createItem);
+	}
+}
+function pageLoad(event) {
+	firebase.on("value", dataChanged);
+
+// 	if (localStorage.getItem('receipt') == null) {
+// 		receipt.items.forEach(createItem);
+
+// 	} else {
+
+// 	// receipt = JSON.parse(localStorage.getItem("receipt"));
+// 	// receipt.items.forEach(createItem)}
+// 	}
+}
+
+
+function enter(event) {
+	event.preventDefault(event)
+	// stores on what is entered into the box
+	var inputValue = parseFloat(input.value);
+
+	receipt.items.push(inputValue);
+
+	// localStorage.setItem('receipt', JSON.stringify(receipt));
+	firebase.set((receipt));
+
+	createItem(inputValue);
+
+}
+
+function createItem(amount) {
+	// stores new entry into a list element
+	var newPrice = document.createElement("li");
+	// adds the newprice variable into the entries
+	entries.appendChild(newPrice);
+
+	// the new enry is equal to $ plus what was entered in box
+	newPrice.textContent = formatCurrency(amount);
+	
+	// displays the total as the $ + what was entered in total value
+	total.textContent = formatCurrency(totalValue);
+
+	// total value is equal to the previous total plus the new entry
+	totalValue = totalValue + amount;
+
+	input.value ="";
+
+}
+
+
+function formatCurrency(number){
+	var currency = parseFloat(number);
+	currency = currency.toFixed(2);
+	currency = "$" + currency;
+	return currency;
+
+}
--- dessert-guide/js/javascript.js
+++ dessert-guide/js/javascript.js
@@ -1,3 +1,5 @@
+console.log('this works');
+
 var markers = [
     {
         "title": 'twisted donuts',
@@ -7,7 +9,7 @@ var markers = [
     },
 
     {
-        "title": 'mitchells ice cream',
+        "title": 'mitchells-ice-cream',
         "lat": '37.74415',
         "lng": '-122.42282',
         "description": "<h3 align='center'>mitchell's ice cream</h3> <p align='center'>1243 noriega street<br> san francisco, ca 94122</p>"
@@ -22,149 +24,149 @@ var markers = [
 
 	{
         "title": 'pineapples',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.72326',  
+        "lng": '-122.44299',
         "description": "<h3 align='center'>pineapples</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
-        "title": 'poly ann',
-        "lat": '',  
-        "lng": '',
+        "title": 'poly-ann',
+        "lat": '37.75347',  
+        "lng": '-122.49771',
         "description": "<h3 align='center'>polly ann ice cream</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
-        "title": 'humphrey',
-        "lat": '',  
-        "lng": '',
+        "title": 'humphry',
+        "lat": '37.7528',  
+        "lng": '-122.41193',
         "description": "<h3 align='center'>humphry slocombe</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
     {
         "title": 'fentons',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.82794',  
+        "lng": '-122.24999',
         "description": "<h3 align='center'>fentons creamery</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
     {
-        "title": "peter's kettle corn",
-        "lat": '',  
-        "lng": '',
+        "title": "peters-kettle",
+        "lat": '37.78889',  
+        "lng": '-122.19657',
         "description": "<h3 align='center'>peter's kettle corn</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
         "title": 'smitten',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.77635',  
+        "lng": '-122.4242',
         "description": "<h3 align='center'>smitten ice cream</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
         "title": 'cream',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.76505',  
+        "lng": '-122.42221',
         "description": "<h3 align='center'>cream</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
-        "title": 'genki crepe',
-        "lat": '',  
-        "lng": '',
+        "title": 'genki',
+        "lat": '37.78316',  
+        "lng": '-122.46286',
         "description": "<h3 align='center'>genki crepes</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
 	{
         "title": 'benkyodo',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.78635',  
+        "lng": '-122.43016',
         "description": "<h3 align='center'>benkyodo</h3> <p align='center'><br>san francisco, ca </p>"
     },
 
     {
         "title": 'plentea',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.79137',  
+        "lng": '-122.4044',
         "description": "<h3 align='center'>plentea</h3> <p align='center'><br>san francisco, ca </p>"
     },
 	{
-        "title": 'the ice cream bar',
-        "lat": '',  
-        "lng": '',
+        "title": 'ice-bar',
+        "lat": '37.76645',  
+        "lng": '-122.45024',
         "description": "<h3 align='center'>the ice cream bar</h3> <p align='center'><br>san francisco, ca </p>"
     },
 	{
         "title": 'tpumps',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.76371',  
+        "lng": '-122.47861',
         "description": "<h3 align='center'>tpumps</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'marco polo',
-        "lat": '',  
-        "lng": '',
+        "title": 'marco-polo',
+        "lat": '37.74261',  
+        "lng": '-122.48187',
         "description": "<h3 align='center'>marco polo italian ice cream</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'bi-rite creamery',
-        "lat": '',  
-        "lng": '',
+        "title": 'birite',
+        "lat": '37.76158',  
+        "lng": '-122.42566',
         "description": "<h3 align='center'>bi-rite creamery</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": "anthony's cookies",
-        "lat": '',  
-        "lng": '',
+        "title": "ants",
+        "lat": '37.7502',  
+        "lng": '-122.42023',
         "description": "<h3 align='center'>anthony's cookies</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'frozen khusterd',
-        "lat": '',  
-        "lng": '',
+        "title": 'frozen',
+        "lat": '37.80676',  
+        "lng": '-122.43147',
         "description": "<h3 align='center'>frozen khusterd</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'pacific puffs',
-        "lat": '',  
-        "lng": '',
+        "title": 'pacific',
+        "lat": '37.79694',  
+        "lng": '-122.43565',
         "description": "<h3 align='center'>pacific puffs</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
         "title": 'straw',
-        "lat": '',  
-        "lng": '',
+        "lat": '37.77404',  
+        "lng": '-122.42424',
         "description": "<h3 align='center'>straw</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'at&t park',
-        "lat": '',  
-        "lng": '',
+        "title": 'att',
+        "lat": '37.77859',  
+        "lng": '-122.38926',
         "description": "<h3 align='center'>at&t park: ghiradelli</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'holy gelato!',
-        "lat": '',  
-        "lng": '',
+        "title": 'holy',
+        "lat": '37.76233',  
+        "lng": '-122.46592',
         "description": "<h3 align='center'>holy gelato!</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": "dianda's italian american pasty",
-        "lat": '',  
-        "lng": '',
+        "title": "diandas",
+        "lat": '37.75097',  
+        "lng": '-122.41817',
         "description": "<h3 align='center'>dianda's italian american pastry</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
-        "title": 'super cue cafe',
-        "lat": '',  
-        "lng": '',
+        "title": 'super',
+        "lat": '37.72427',  
+        "lng": '-122.45701',
         "description": "<h3 align='center'>super cue cafe</h3> <p align='center'><br>san francisco, ca </p>"
     },
     {
         "title": "sophie's crepes",
-        "lat": '',  
-        "lng": '',
+        "lat": '37.78508',  
+        "lng": '-122.4321',
         "description": "<h3 align='center'>sophie's crepe's</h3> <p align='center'><br>san francisco, ca </p>"
     }
 
@@ -174,64 +176,72 @@ var markers = [
     var labelIndex = 0;
 
     window.onload = function () {
-        LoadMap();
+        initMap();
     }
-    function LoadMap() {
+
+    function initMap() {
         var mapOptions = {
-            center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
-            zoom: 11,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            center: {lat: 37.7704412, lng: -122.364841},
+            zoom: 12,
+            // mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("bayMap"), mapOptions);
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
  
-        //Create and open InfoWindow.
-        var infoWindow = new google.maps.InfoWindow();
+        // Create and open InfoWindow.
+        // var infoWindow = new google.maps.InfoWindow();
  
         for (var i = 0; i < markers.length; i++) {
             var data = markers[i];
             var myLatlng = new google.maps.LatLng(data.lat, data.lng);
-            var marker = new google.maps.Marker({
+            var marker = new google.maps.Marker ({
                 position: myLatlng,
                 map: map,
                 title: data.title,
                 label: labels[labelIndex++ % labels.length]
 
             });
-
+          }
+      }
             //Attach click event to the marker.
-            (function (marker, data) {
+            function click (markers, data) {
+            	console.log(marker.title);
                 google.maps.event.addListener(marker, "click", function (e) {
-                    //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
-                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
-                    infoWindow.open(map, marker);
-                });
-            })(marker, data);
-        }
+                	var marker_target = e.Gb.target.title;
+                	var dessert_target = data.title;
 
+                	marker_target.forEach(function (element) {
+                		console.log(element);
+                	});
+                	
+                	// document.anchors.name;
+
+                	document.anchors.forEach(function (element){
+                		console.log(element);
+                	});
+                });
+               }
+
+                    //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                    // infoWindow.setContent("<div style = 'width:150px;min-height:40px'>" + data.description + "</div>");
+                    // infoWindow.open(map, marker);
+                // });
+
+
+// click event that links to side bar
         // function (marker, data) {
         // 	google.maps.event.addEventListener(marker, "click", function (e) 
-    }
+  
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// 	function initMap() {
-// 	  var map = new google.maps.Map(document.getElementById('map'), {
-// 	    center: {lat: 37.76645, lng: -122.45024},
-// 	    zoom: 12
-// 	  });
+	// function initMap() {
+	//   var map = new google.maps.Map(document.getElementById('map'), {
+	//     center: {lat: 37.76645, lng: -122.45024},
+	//     zoom: 12
+	//   });
 
 // //    // // a new Info Window is created
 // //    // infoWindow = new google.maps.InfoWindow();
