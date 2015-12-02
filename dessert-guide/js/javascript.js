8e1451a46c9bfb07e5cac049411ee2e4745fcd6c
Leah Filamor <leahfilamor@gmail.com>
2015-12-02 01:38:17 -0800

    almost done : )

--- dessert-guide/js/javascript.js
+++ dessert-guide/js/javascript.js
@@ -1,173 +1,172 @@
-console.log('this works');
 
 var markers = [
     {
         "title": 'twisted',
         "lat": '37.75403',
         "lng": '-122.47724',
-        "description": '<a href="#twisted"><h3 align="center">twisted donuts</h3></a> <p align="center">1243 noriega street<br> san francisco, ca 94122</p>'
+        "description": '<a href="#twisted"><h3 align="center">twisted donuts</h3></a><p align="center">1243 noriega street<br> san francisco, ca 94122</p>'
     },
 
     {
         "title": 'mitchells-ice-cream',
         "lat": '37.74415',
         "lng": '-122.42282',
-        "description": "<h3 align='center'>mitchell's ice cream</h3> <p align='center'>1243 noriega street<br> san francisco, ca 94122</p>"
+        "description": "<a href='#mitchells-ice-cream'><h3 align='center'>mitchell's ice cream</h3></a><p align='center'>1243 noriega street<br> san francisco, ca 94122</p>"
     },
 
 	{
         "title": 'outerlands',
         "lat": '37.76026',  
         "lng": '-122.50502',
-        "description": "<h3 align='center'>outerlands</h3> <p align='center'>4001 judah street<br>san francisco, ca 94122</p>"
+        "description": "<a href='#outerlands'><h3 align='center'>outerlands</h3></a><p align='center'>4001 judah street<br>san francisco, ca 94122</p>"
     },
 
 	{
         "title": 'pineapples',
         "lat": '37.72326',  
         "lng": '-122.44299',
-        "description": "<h3 align='center'>pineapples</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#pineapples'><h3 align='center'>pineapples</h3></a><p align='center'>296 ocean ave<br>san francisco, ca </p>"
     },
 
 	{
         "title": 'poly-ann',
         "lat": '37.75347',  
         "lng": '-122.49771',
-        "description": "<h3 align='center'>polly ann ice cream</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#poly-ann'><h3 align='center'>polly ann ice cream</h3></a><p align='center'>3138 noriega street<br>san francisco, ca </p>"
     },
 
 	{
         "title": 'humphry',
         "lat": '37.7528',  
         "lng": '-122.41193',
-        "description": "<h3 align='center'>humphry slocombe</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#humphry'><h3 align='center'>humphry slocombe</h3></a><p align='center'>2790 harrison st<br>san francisco, ca </p>"
     },
 
     {
         "title": 'fentons',
         "lat": '37.82794',  
         "lng": '-122.24999',
-        "description": "<h3 align='center'>fentons creamery</h3> <p align='center'><br>oakland, ca </p>"
+        "description": "<a href='#fentons'><h3 align='center'>fentons creamery</h3></a><p align='center'>4226 piedmont ave<br>oakland, ca </p>"
     },
 
     {
         "title": "peters-kettle",
         "lat": '37.78889',  
         "lng": '-122.19657',
-        "description": "<h3 align='center'>peter's kettle corn</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#peters-kettle'><h3 align='center'>peter's kettle corn</h3></a><p align='center'4139 macarthur blvd><br>oakland, ca </p>"
     },
 
 	{
         "title": 'smitten',
         "lat": '37.77635',  
         "lng": '-122.4242',
-        "description": "<h3 align='center'>smitten ice cream</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#smitten'><h3 align='center'>smitten ice cream</h3></a><p align='center'>432 octavia st<br>san francisco, ca </p>"
     },
 
 	{
         "title": 'cream',
         "lat": '37.76505',  
         "lng": '-122.42221',
-        "description": "<h3 align='center'>cream</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#cream'><h3 align='center'>cream</h3></a><p align='center'>2399 telegraph ave<br>berkeley, ca </p>"
     },
 
 	{
         "title": 'genki',
         "lat": '37.78316',  
         "lng": '-122.46286',
-        "description": "<h3 align='center'>genki crepes</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#genki'><h3 align='center'>genki crepes</h3></a><p align='center'>330 clement st<br>san francisco, ca </p>"
     },
 
 	{
         "title": 'benkyodo',
         "lat": '37.78635',  
         "lng": '-122.43016',
-        "description": "<h3 align='center'>benkyodo</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#benkyodo'><h3 align='center'>benkyodo</h3></a><p align='center'>1747 buchanan st<br>san francisco, ca </p>"
     },
 
     {
         "title": 'plentea',
         "lat": '37.79137',  
         "lng": '-122.4044',
-        "description": "<h3 align='center'>plentea</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#plentea'><h3 align='center'>plentea</h3></a><p align='center'>314 kearny st<br>san francisco, ca </p>"
     },
 	{
         "title": 'ice-bar',
         "lat": '37.76645',  
         "lng": '-122.45024',
-        "description": "<h3 align='center'>the ice cream bar</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#ice-bar'><h3 align='center'>the ice cream bar</h3></a><p align='center'>815 cole st<br>san francisco, ca </p>"
     },
 	{
         "title": 'tpumps',
         "lat": '37.76371',  
         "lng": '-122.47861',
-        "description": "<h3 align='center'>tpumps</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#tpumps'><h3 align='center'>tpumps</h3></a><p align='center'>1916 irving st<br>san francisco, ca </p>"
     },
     {
         "title": 'marco-polo',
         "lat": '37.74261',  
         "lng": '-122.48187',
-        "description": "<h3 align='center'>marco polo italian ice cream</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#marco-polo'><h3 align='center'>marco polo italian ice cream</h3></a><p align='center'>1447 taravel st<br>san francisco, ca </p>"
     },
     {
         "title": 'birite',
         "lat": '37.76158',  
         "lng": '-122.42566',
-        "description": "<h3 align='center'>bi-rite creamery</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#'birite><h3 align='center'>bi-rite creamery</h3></a><p align='center'>3692 18th st<br>san francisco, ca </p>"
     },
     {
         "title": "ants",
         "lat": '37.7502',  
         "lng": '-122.42023',
-        "description": "<h3 align='center'>anthony's cookies</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#ants'><h3 align='center'>anthony's cookies</h3></a><p align='center'>1417 valencia st<br>san francisco, ca </p>"
     },
     {
         "title": 'frozen',
         "lat": '37.80676',  
         "lng": '-122.43147',
-        "description": "<h3 align='center'>frozen khusterd</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#frozen'><h3 align='center'>frozen khusterd</h3></a><p align='center'>2 marina blvd<br>san francisco, ca </p>"
     },
     {
         "title": 'pacific',
         "lat": '37.79694',  
         "lng": '-122.43565',
-        "description": "<h3 align='center'>pacific puffs</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#pacific'><h3 align='center'>pacific puffs</h3></a><p align='center'>2201 union st<br>san francisco, ca </p>"
     },
     {
         "title": 'straw',
         "lat": '37.77404',  
         "lng": '-122.42424',
-        "description": "<h3 align='center'>straw</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#straw'><h3 align='center'>straw</h3></a><p align='center'>203 octavia blvd<br>san francisco, ca </p>"
     },
     {
         "title": 'att',
         "lat": '37.77859',  
         "lng": '-122.38926',
-        "description": "<h3 align='center'>at&t park: ghiradelli</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#att'><h3 align='center'>at&t park: ghiradelli</h3></a><p align='center'>24 willie mays plaza<br>san francisco, ca </p>"
     },
     {
         "title": 'holy',
         "lat": '37.76233',  
         "lng": '-122.46592',
-        "description": "<h3 align='center'>holy gelato!</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#holy'><h3 align='center'>holy gelato!</h3></a><p align='center'>1392 9th ave<br>san francisco, ca </p>"
     },
     {
         "title": "diandas",
         "lat": '37.75097',  
         "lng": '-122.41817',
-        "description": "<h3 align='center'>dianda's italian american pastry</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#diandas'><h3 align='center'>dianda's italian american pastry</h3></a><p align='center'>2883 mission st<br>san francisco, ca </p>"
     },
     {
         "title": 'super',
         "lat": '37.72427',  
         "lng": '-122.45701',
-        "description": "<h3 align='center'>super cue cafe</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#super'><h3 align='center'>super cue cafe</h3></a><p align='center'>1139 taravel st<br>san francisco, ca </p>"
     },
     {
-        "title": "sophie's crepes",
+        "title": "sophies-crepes",
         "lat": '37.78508',  
         "lng": '-122.4321',
-        "description": "<h3 align='center'>sophie's crepe's</h3> <p align='center'><br>san francisco, ca </p>"
+        "description": "<a href='#sophies-crepes'><h3 align='center'>sophie's crepe's</h3></a><p align='center'>1581 webster st<br>san francisco, ca </p>"
     }
 
     ];
@@ -181,7 +180,7 @@ var markers = [
 
     function initMap() {
         var mapOptions = {
-            center: {lat: 37.7704412, lng: -122.364841},
+            center: {lat: 37.7598564, lng: -122.3785739,},
             zoom: 12,
             // mapTypeId: google.maps.MapTypeId.ROADMAP
         };
@@ -206,12 +205,13 @@ var markers = [
 
             });
 
+
             //Attach click event to the marker.
             (function (markers, data) {
-                console.log(marker.title);
+                // console.log(marker.title);
 
                 marker.addListener("click", function (e) {
-                    console.log('this is inside');
+                    // console.log('this is inside');
                     var marker_target = e.Gb.target.title;
                     var dessert_target = data.title;
 
@@ -223,7 +223,7 @@ var markers = [
                     //     console.log(element);
                     // });
 
-                    infoWindow.setContent("<div style = 'width:150px;min-height:40px'>" + data.description + "</div>");
+                    infoWindow.setContent("<div class='infostyle' style = 'width:150px;min-height:40px'>" + data.description + "</div>");
                     infoWindow.open(map, marker);
                 });
 
@@ -231,173 +231,10 @@ var markers = [
 
           }
 
-
-      }
-
-                
-
-// function jump (e) {
-//     var sections = location.href;
-//     location.href = "#"+e;
-//     document.querySelector.scroll
-// }
-// click event that links to side bar
-        // function (marker, data) {
-        // 	google.maps.event.addEventListener(marker, "click", function (e) 
-  
-
-
-
-
-
-
-	// function initMap() {
-	//   var map = new google.maps.Map(document.getElementById('map'), {
-	//     center: {lat: 37.76645, lng: -122.45024},
-	//     zoom: 12
-	//   });
-
-// //    // // a new Info Window is created
-// //    // infoWindow = new google.maps.InfoWindow();
-
-// //    // // Event that closes the InfoWindow with a click on the map
-// //    // google.maps.event.addListener(map, 'click', function() {
-// //    //    infoWindow.close();
-// //    // });
-
-// //    // Finally displayMarkers() function is called to begin the markers creation
-// //    displayMarkers();
-// // }
-
-	  
-// // 	// var myLatLng = {lat: 37.76645, lng: -122.45024};
-
-
-
-// 	var latLngArray = [
-
-// 		{
-// 			lat: 37.76645,
-// 		 	lng: -122.45024
-// 		 },
-
-// 		{
-// 			lat: 37.77584, 
-// 			lng: -122.49778
-
-// 		}
-
-// 	];
-
-// // 	function displayMarkers() {
-// //   	 var bounds = new google.maps.LatLngBounds();
-
-// // 	for (var i = 0; i < latLngArray.length; i++){
-
-// //       var latlng = new google.maps.LatLng(latLngArray[i].lat, latLngArray[i].lng);
-// //       // var name = markersData[i].name;
-
-// //   createMarker(latlng);
-
-// //   bounds.extend(latlng);
-// //   map.fitBounds(bounds);
-// // }}
-
-// // function createMarker(latlng) {
-// // 	var marker = new google.maps.Marker({
-// //     position: latlng,
-// //     map: map,
-// //   });
-
-
-// //   google.maps.event.addListener(marker, 'click', function() {
+}
       
-// // var iwContent = '<div id="iw_container">' +
-// //       '<div class="iw_title">' + name + '</div>' +
-// //       '<div class="iw_content">' + address1 + '<br />' +
-// //       address2 + '<br />' +
-// //       postalCode + '</div></div>';
-      
-// //       // including content to the infowindow
-// //       infoWindow.setContent(iwContent);
 
-// //       // opening the infowindow in the current map and at the current marker location
-// //       infoWindow.open(map, marker);
-// //    });
-// // }
-
-
-
-
-// // var layer = new google.maps.KmlLayer ({
-// // 	url: 'http://leahyousquare.github.io/fewd-36/dessert-guide/kml/desserts1.kml',
-// // 	    suppressInfoWindows: true,
-// // 	      preserveViewport: false,
-// // 	map: map
-// // });
-
-
-
-// // var myCenter = new google.maps.LatLng(37.7616486, -122.4866023);
-// // var marker= new google.maps.Marker({
-// //   position:myCenter,
-// //   });
-
-// // marker.setMap(map);
-
-// // google.maps.event.addDomListener(window, 'load', initialize);
-// // }
-
-// // // these two variables are global so they can be accessed inside the functions
-// 	// var map;
-// 	// var kmlUrl = 'http://leahyousquare.github.io/fewd-36/dessert-guide/kml/desserts1.kml';
-
-// 	// function initMap() {
-// 	//   var map = new google.maps.Map(document.getElementById('map'), {
-// 	//     center: {lat: 37.7616486, lng: -122.4866023},
-// 	//     zoom: 13
-// 	//   });
-// 	//   loadKmlLayer(kmlUrl, map)
-// 	// }
-
-// 	// function loadKmlLayer(kmlUrl, map) {
-// 	// 	var kmlLayer = new google.maps.KmlLayer(kmlUrl, {
-// 	// 		suppressInfoWindows: true,
-// 	// 		preserveViewport: false,
-// // 	// 		map: map
-// // 	// 	});
-// // 	// }
-
-
-
-// // var allView = document.querySelector(".all");
-// // var oneView = document.querySelector(".one");
-
-// // allView.addEventListener("click", seeAll);
-// // oneView.addEventListener("click", seeOne);
-
-
-// // 	// all listed view
-// // function seeAll(event) {
-// // 	body.className = "allAtOnce";
-// // }
-
-// // function seeOne(event) {
-// // 	body.className = "onlyOne";
-// // }
-
-// // 	// list view
-
-
-
-// 	// map interaction js
-
-
-
-
-
-
-
+   
 
 
 
